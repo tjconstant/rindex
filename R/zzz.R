7a142cccaf689142f5d9f659c2b31f2bb757f36f
Tom Constant <t.j.constant@gmail.com>
2018-04-25 17:00:13 +0100

    added a check on first run if the database needs to be installed.

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function(libname, pkgname){
+
+  test_file <- "extdata/refractiveindex.info-database-master/database/library.yml"
+
+  if(!file.exists(system.file(test_file, package = "rindex"))){
+    message("Installing database for first-time loading of package.")
+    install_database()
+  }
+
+}
