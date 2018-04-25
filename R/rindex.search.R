0bc1f7c70685c260dfa9b4f4ed177519b772770b
Tom Constant <t.j.constant@gmail.com>
2018-04-25 23:55:38 +0100

    fiddling with search (not working as well as master yet)

--- R/rindex.search.R
+++ R/rindex.search.R
@@ -9,18 +9,8 @@
 #' rindex.search("Ag")
 #'
 rindex.search <- function(material) {
-  con <- RSQLite::dbConnect(drv = RSQLite::SQLite(), dbname = system.file("extdata", "refractive.db", package = "rindex"))
-  result <-
-    RSQLite::dbGetQuery(con,
-               paste("SELECT * FROM pages WHERE book LIKE '%", material, "%'", sep = ""))
 
-  RSQLite::dbDisconnect(con)
-  material_table <- result[,c(1:4,6:10)]
-  # material_table$rangeMin <- material_table$rangeMin * 1e-6
-  # material_table$rangeMax <- material_table$rangeMax * 1e-6
+  material_table <- db_df[agrep(pattern = material, x = db_df$BOOK),]
 
-  names(material_table)[7] <- "rangeMin (um)"
-  names(material_table)[8] <- "rangeMax (um)"
-
-  return(material_table)
+  return(material_table[,1:4])
 }
